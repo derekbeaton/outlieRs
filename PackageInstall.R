b58db1c252bf69bccf2fe2a489101759b61b18d6
derekbeaton <derek@bbstechs-MacBook-Pro.local>
2017-01-30 14:54:47 -0500

    Update to package install

--- PackageInstall.R
+++ PackageInstall.R
@@ -1,8 +1,17 @@
+## Please make sure that your install of R and/or RStudio are up to date. (at least 3.2.2, preferably 3.3.*)
+
 # Install and Load package from GitHub
 # Run this file only once!
 
+
 install.packages(c('rrcov','doparallel','TInPosition','rpca','reshape','ggplot2','colorspace','robustbase'),dependencies = T)
 
-install.packages("devtools")
-require(devtools)
-install_github("derekbeaton/outlieRs", subdir="Package")
+
+## INSTALL OPTION (1) 
+	## If the following block of code does not work (i.e., a failure to install certain packages) then use the line of code below this block.
+	install.packages("devtools")
+	require(devtools)
+	install_github("derekbeaton/outlieRs", subdir="Package")
+	
+## INSTALL OPTION (2)
+	install.packages('http://github.com/derekbeaton/outlieRs/raw/master/outlieRs_0.1.0.9000.tar.gz',repos=NULL)
